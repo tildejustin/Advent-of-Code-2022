eced30e0e7b78a1dc203299e45c0f606cbebed20
~justin <spacepepper3.14@gmail.com>
2022-12-01 14:32:24 -0500

    added variable types

---
+++ day1/elf_calories.py
@@ -0,0 +1,19 @@
+with open("input.txt", "r", encoding="utf8") as file:
+    text = file.readlines()
+
+counter: int = 0
+array: list = [[]]
+for number in text:
+    if number == "\n":
+        array.append([])
+    else:
+        # always adds to the latest array
+        array[len(array) - 1].append(int(number))
+
+
+sum_array: list = list(map(sum, array))
+print(max(sum_array))
+
+# part 2
+sum_array.sort()
+print(sum(sum_array[-3:]))
