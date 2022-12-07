7cfd58995a8f6bf4ea4b12f41eacb426195776c7
~justin <spacepepper3.14@gmail.com>
2022-12-07 15:59:24 -0500

    day 2 omgomgomgomgomg

--- day7/run.py
+++ day7/run.py
@@ -22,7 +22,7 @@ class File:
         return "/" + "/".join(self.fpath[1:])
 
 
-with open("example.txt") as f:
+with open("input") as f:
     lines: list = f.readlines()
     current_path = []
     files = []
@@ -48,14 +48,21 @@ for path in all_paths_copy:
     for i in range(1, len(path.split("/"))):
         all_paths.add("/" +  "/".join(path.split("/")[1:i]))
 
-# print(list(all_paths))
-# [print(f.get_string_path()) for f in files]
+dir_sizes = [sum([file.get_size() for file in filter(lambda f: f.get_string_path().startswith(path), files)]) for path in all_paths]
 
-for path in all_paths:
-    filtered_files = list(filter(lambda f: f.get_string_path().startswith(path), files))
-    print(sum([file.get_size() for file in filtered_files]), path)
-    file_sizes = [file.get_size() for file in filtered_files]
-    sum_of_dir = sum(file_sizes)
-    # print(filtered_files, path)
+dir_sizes_two = copy.deepcopy(dir_sizes)
 
-# sum_dir = [file_sizes_per_dir for file_sizes_per_dir in [file_sizes_per_dir for path in all_paths]]
+dir_sizes = [dir_size for dir_size in dir_sizes if dir_size <= 100000]
+print("part 1:", sum(dir_sizes))
+
+# part 2
+# too lazy to write real code
+root_size = USED_SPACE = max(dir_sizes_two)
+SPACE = 70000000
+UNUSED_SPACE  = SPACE - USED_SPACE
+NEED_EMPTY = 30000000
+NEED = NEED_EMPTY - UNUSED_SPACE
+
+dir_sizes_two = [dir_size for dir_size in dir_sizes_two if dir_size >= NEED]
+dir_sizes_two.sort()
+print("part 2:", dir_sizes_two[0])
